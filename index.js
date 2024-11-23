41844f5045cc1e695144b603eaf810da8385c75b
Steamgx <your-bmuraya646@gmail.com>
2024-11-23 23:33:41 +0300

    calculator

---
+++ index.js
@@ -0,0 +1,44 @@
+function add (x,y){
+return x + y;
+  }
+  
+  function subtract(x, y) {
+    return x - y;
+  } 
+    
+  
+  function multiply(x, y) {
+    return x * y;
+  }
+  
+  function divide(a, b) {
+    if (b === 0) {
+      return "Cannot divide by zero!";
+    }
+    return a / b;
+  }
+  
+  function increment(n) {
+    return n + 1;
+  }
+  
+  function decrement(n) {
+    return n - 1;
+  }
+  
+  function makeInt(n) {
+    return parseInt(n, 10);
+  }
+  
+
+  
+  function preserveDecimal(n) {
+    return parseFloat(n);
+  }
+  
+  // Test the function
+  console.log(preserveDecimal("3.1415"));  
+  console.log(preserveDecimal("20.5px"));
+  console.log(preserveDecimal("brian"));  
+  console.log(preserveDecimal("1234"));    
+  console.log(preserveDecimal(".42"));
